95bee876a2f304be4d535ad30d37261281e9782e
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-10-02 13:59:22 +0700

    fix link page + update profile page

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+// import axios from "axios";
 
-const config = {
-    baseURL: "https://localhost:5145/api/",
-}
+// const config = {
+//     baseURL: "https://localhost:5145/api/",
+// }
 
-const api = axios.create(config);
+// const api = axios.create(config);
 
-// Apply the interceptor to the api instance only
-api.interceptors.request.use(function (config) {
-    // Do something before request is sent
-    console.log("Sending request to API:", config.url);
-    return config;
-}, function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-});
+// // Apply the interceptor to the api instance only
+// api.interceptors.request.use(function (config) {
+//     // Do something before request is sent
+//     console.log("Sending request to API:", config.url);
+//     return config;
+// }, function (error) {
+//     // Do something with request error
+//     return Promise.reject(error);
+// });
 
-export default api;
+// export default api;

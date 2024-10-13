907c1c7f37892f0379444061a4a7e6856cc2cf2c
beurlpng <nhuhtyse171830@fpt.edu.vn>
2024-10-13 15:34:27 +0700

    update dashboard config

--- src/pages/dashboard/layout/main-dashboard/Main.jsx
+++ src/pages/dashboard/layout/main-dashboard/Main.jsx
@@ -1,25 +1,9 @@
-import { useState, useEffect } from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import { Layout, Drawer, Affix, Card } from "antd";
-import SideNav from "../side-nav/SideNav";
-import Header from "../header-dasboard/Header";
+import { Layout } from "antd";
+
 import "./Main.scss";
-import { LeftCircleFilled, LeftCircleTwoTone } from "@ant-design/icons";
-import { AiOutlineLogout } from "react-icons/ai";
 
-const { Content, Sider } = Layout;
-
-function Main({ children }) {
-  const [isExpand, setIsExpand] = useState(false);
-  const [visible, setVisible] = useState(false);
-
- 
-
-  return (
-    <Layout className="layout-dashboard">
-     
-    </Layout>
-  );
+function Main() {
+  return <Layout className="layout-dashboard"></Layout>;
 }
 
 export default Main;

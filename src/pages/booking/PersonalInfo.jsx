16d249543f6cecfe336f8d3ec301b11210ce2856
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-10-21 00:04:17 +0700

    update api salon

--- src/pages/booking/PersonalInfo.jsx
+++ src/pages/booking/PersonalInfo.jsx
@@ -5,10 +5,11 @@ import {
   RightOutlined,
   UserOutlined,
 } from "@ant-design/icons";
-import { Button, Card, ConfigProvider, Input } from "antd";
-import React from "react";
+import { Button, Card, ConfigProvider, Input, message } from "antd";
+import React, { useEffect, useState } from "react";
 import "./Booking.scss";
 import Slider from "react-slick/lib/slider";
+import api from "../../config/axios";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -33,22 +34,38 @@ function SamplePrevArrow(props) {
 }
 
 function PersonalInfo({ personalInfo, setPersonalInfo, onNext }) {
+  const [salons, setSalons] = useState([]);
   const handleChange = (e) => {
     setPersonalInfo({ ...personalInfo, [e.target.name]: e.target.value });
   };
 
+  const getHairSalon = async () => {
+    try {
+      const response = await api.get("/Salon/PrintAllSalon");
+      const data = response.data.data;
+      console.log(data);
+      setSalons(data);
+    } catch (error) {
+      message.error(data.message);
+    }
+  }
+  useEffect(() => {
+    getHairSalon();
+  }, []);
+
   const settings = {
-    infinite: true,
-    slidesToShow: 2,
+    infinite: salons.length > 1,  
+    slidesToShow: Math.min(2, salons.length),  
     slidesToScroll: 1,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
-    autoplay: true,
+    autoplay: salons.length > 1, 
     autoplaySpeed: 6000,
     pauseOnHover: true,
     centerMode: true,
     centerPadding: "0",
   };
+  
 
   return (
     <div className="personalInfo">
@@ -73,151 +90,52 @@ function PersonalInfo({ personalInfo, setPersonalInfo, onNext }) {
           prefix={<GlobalOutlined />}
         />
         <div className="slider-container">
-          <Slider {...settings}>
-            <div>
-              <Card
-                cover={
-                  <img
-                    alt="example"
-                    src="https://images.squarespace-cdn.com/content/v1/5b4818cdd274cba3ecbed6f9/1556647673075-QP3E1VHA2Y9388XA8RV9/20190317_ASalon_Interior_001.JPG"
-                  />
-                }
-                className="personalInfo__card"
-              >
-                <p className="personalInfo__card__title">Hair Salon 1</p>
-                <p className="personalInfo__card__subtitle">
-                  Location: 1 ABC Street, District H
-                </p>
-                <ConfigProvider
-                  theme={{
-                    components: {
-                      Button: {
-                        defaultColor: "black",
-                        defaultBg: "#FAA300",
-                        defaultBorderColor: "#FAA300",
-                        defaultHoverBorderColor: "black",
-                        defaultHoverColor: "white",
-                        defaultHoverBg: "black",
-                        defaultActiveBg: "#FAA300",
-                        defaultActiveBorderColor: "#FAA300",
-                        defaultActiveColor: "black",
-                      },
-                    },
-                  }}
-                >
-                  <Button>Choose this place</Button>
-                </ConfigProvider>
-              </Card>
-            </div>
-            <div>
-              <Card
-                cover={
-                  <img
-                    alt="example"
-                    src="https://clayhairsalon.com/wp-content/uploads/2022/06/The-Clay-Way-scaled-1-1536x1025.jpg"
-                  />
-                }
-                className="personalInfo__card"
-              >
-                <p className="personalInfo__card__title">Hair Salon 1</p>
-                <p className="personalInfo__card__subtitle">
-                  Location: 1 ABC Street, District H
-                </p>
-                <ConfigProvider
-                  theme={{
-                    components: {
-                      Button: {
-                        defaultColor: "black",
-                        defaultBg: "#FAA300",
-                        defaultBorderColor: "#FAA300",
-                        defaultHoverBorderColor: "black",
-                        defaultHoverColor: "white",
-                        defaultHoverBg: "black",
-                        defaultActiveBg: "#FAA300",
-                        defaultActiveBorderColor: "#FAA300",
-                        defaultActiveColor: "black",
-                      },
-                    },
-                  }}
-                >
-                  <Button>Choose this place</Button>
-                </ConfigProvider>
-              </Card>
-            </div>
-            <div>
-              <Card
-                cover={
-                  <img
-                    alt="example"
-                    src="https://e7nmeemxs8h.exactdn.com/wp-content/uploads/2023/12/kings-road-home-24.jpg?lossy=1&ssl=1"
-                  />
-                }
-                className="personalInfo__card"
-              >
-                <p className="personalInfo__card__title">Hair Salon 1</p>
-                <p className="personalInfo__card__subtitle">
-                  Location: 1 ABC Street, District H
-                </p>
-                <ConfigProvider
-                  theme={{
-                    components: {
-                      Button: {
-                        defaultColor: "black",
-                        defaultBg: "#FAA300",
-                        defaultBorderColor: "#FAA300",
-                        defaultHoverBorderColor: "black",
-                        defaultHoverColor: "white",
-                        defaultHoverBg: "black",
-                        defaultActiveBg: "#FAA300",
-                        defaultActiveBorderColor: "#FAA300",
-                        defaultActiveColor: "black",
-                      },
-                    },
-                  }}
-                >
-                  <Button>Choose this place</Button>
-                </ConfigProvider>
-              </Card>
-            </div>
-            <div>
-              <Card
-                cover={
-                  <img
-                    alt="example"
-                    src=" https://avenuemspa.com/wp-content/uploads/2016/09/main-page-hair-services-link-and-also-photo-on-hair-services-page.jpg"
-                  />
-                }
-                className="personalInfo__card"
-              >
-                <p className="personalInfo__card__title">Hair Salon 1</p>
-                <p className="personalInfo__card__subtitle">
-                  Location: 1 ABC Street, District H
-                </p>
-                <ConfigProvider
-                  theme={{
-                    components: {
-                      Button: {
-                        defaultColor: "black",
-                        defaultBg: "#FAA300",
-                        defaultBorderColor: "#FAA300",
-                        defaultHoverBorderColor: "black",
-                        defaultHoverColor: "white",
-                        defaultHoverBg: "black",
-                        defaultActiveBg: "#FAA300",
-                        defaultActiveBorderColor: "#FAA300",
-                        defaultActiveColor: "black",
-                      },
-                    },
-                  }}
-                >
-                  <Button>Choose this place</Button>
-                </ConfigProvider>
-              </Card>
-            </div>
-          </Slider>
+          {salons.length > 0 ? (
+            <Slider {...settings}>
+              {salons.map((salon) => (
+                <div key={salon.salonId}>
+                  <Card
+                    cover={
+                      <img
+                        alt={salon.salonName}
+                        src={salon.image} 
+                      />
+                    }
+                    className="personalInfo__card"
+                  >
+                    <p className="personalInfo__card__title">{salon.salonName}</p>
+                    <p className="personalInfo__card__subtitle">Location: {salon.address}</p>
+                    <ConfigProvider
+                      theme={{
+                        components: {
+                          Button: {
+                            defaultColor: "black",
+                            defaultBg: "#FAA300",
+                            defaultBorderColor: "#FAA300",
+                            defaultHoverBorderColor: "black",
+                            defaultHoverColor: "white",
+                            defaultHoverBg: "black",
+                            defaultActiveBg: "#FAA300",
+                            defaultActiveBorderColor: "#FAA300",
+                            defaultActiveColor: "black",
+                          },
+                        },
+                      }}
+                    >
+                      <Button onClick={() => onNext(salon.salonId)}>
+                        Choose this place
+                      </Button>
+                    </ConfigProvider>
+                  </Card>
+                </div>
+              ))}
+            </Slider>
+          ) : (
+            <p>Loading salons...</p>
+          )}
         </div>
       </div>
-     
+
     </div>
   );
 }

a652da3ef9c063b69838fd4aac39c8104e360674
hathynn <jennyhatyn@gmail.com>
2024-11-08 13:55:46 +0700

    fix ui

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -56,7 +56,7 @@ function Header() {
             </Link>
             <Flex className="header__item__menu">
               <p onClick={() => nav("/")}>Home</p>
-              <p onClick={() => nav("/services")}>Services</p>
+              {/* <p onClick={() => nav("/services")}>Services</p> */}
               <p onClick={() => nav("/feedback")}>Feedback</p>
               <p onClick={() => nav("/about-us")}>About us</p>
             </Flex>
